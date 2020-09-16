2b3537ed15e9ce6f4420ffb17866434cf14d364f
Gautham J <Gauthamastro@gmail.com>
2020-09-16 22:33:21 +0530

    Started Refactoring DEX Module, TODO: -Change chain_spec.rs to have 1UNIT Currency -Replace Native Currency with Generic Asset Pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -6,19 +6,18 @@
 
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
 use frame_system::ensure_signed;
-// use binary_heap_plus::{BinaryHeap, MaxComparator};
+use codec::{Encode,Decode};
 use sp_std::str;
 use sp_std::vec::Vec;
-use substrate_fixed::types::U32F32;
+use sp_arithmetic::FixedU128;
+use sp_std::collections::vec_deque::VecDeque;
 
-mod binary_heap;
-mod engine;
-
-#[cfg(test)]
+#[test]
 mod mock;
 
-#[cfg(test)]
+#[test]
 mod tests;
+
 
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
@@ -49,21 +48,24 @@ decl_error! {
 	}
 }
 
-// The pallet's runtime storage items.
-// https://substrate.dev/docs/en/knowledgebase/runtime/storage
+
 decl_storage! {
-	// A unique name is used to ensure that the pallet's storage items are isolated.
-	// This name may be updated, but each pallet in the runtime must use a unique name.
-	// ---------------------------------vvvvvvvvvvvvvv
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Learn more about declaring storage items:
-		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
 
-		/// Storage items related to DEX Starts here
-		Books get(fn books): map hasher(blake2_128_concat) u32 => engine::OrderBook<T::AccountId>;
+	trait Store for Module<T: Trait> as DEXModule {
 
-		// OrdersByAccountId get(fn orders_by_accountId):
+	// Stores all the different price levels for all the trading pairs in a DoubleMap.
+	PriceLevels get(fn get_pricelevels): double_map hasher(identity) T::Hash, hasher(blake2_128_concat) FixedU128 => LinkedPriceLevel<T>;
+	// Stores all the different active ask and bid levels in the system as a sorted vector mapped to it's TradingPair.
+	// Regarding Performance using sort_unstable, it is in O(nlogn).
+	AsksLevels get(fn get_askslevels): map hasher(identity) T::Hash => Vec<FixedU128>;
+	BidsLevels get(fn get_bidslevels): map hasher(identity) T::Hash => Vec<FixedU128>;
+	// Stores the Orderbook struct for all available trading pairs.
+	Orderbooks get(fn get_orderbooks): map hasher(identity) T::Hash => Orderbook<T>;
+	// Store MarketData of TradingPairs
+	// If the market data is returning None, then no trades were present for that trading in that block.
+	// TODO: Currently we store market data for all the blocks
+	MarketInfo get(fn get_marketdata): double_map hasher(identity) T::Hash, hasher(blake2_128_concat) T::BlockNumber => Option<MarketData>;
+
 	}
 }
 
@@ -82,68 +84,78 @@ decl_module! {
 		// TODO: Note for enabling feeless trades use dispatch::DispatchResultWithPostInfo
 		// TODO: then in the Ok(()) replace it with Ok(Some(0).into()) to make it fee-less
 
-		// This is used to list a new trading pair in the DEX. The origin has to reserve the
-		// TokenListingFee + PairListingFee if the token is not already available in DEX else
-		// only the PairListingFee is reserved until the token is de-listed from the DEX.
-		// Origin will not have any interest. It will avoid abusing the DEX with invaluable tokens
-		// and trading pairs.
-		#[weight = 10000]
-		pub fn register_new_orderbook(origin, trading_asset_id: u32, quote_asset_id: u32) -> dispatch::DispatchResult{
-		let _trader = ensure_signed(origin)?;
-		// TODO: Save the AssetIds check if it's valid and create the the orderbook for the given
-		// TODO: pair
-
-		Ok(())
-		}
-
-		// This function can be used to submit limit orders
-		#[weight = 10000]
-		pub fn submit_limit_order(origin,
-		  order_type: engine::OrderType,
-		  price: u128,
-		  quantity: u128 ) -> dispatch::DispatchResult{
-		let _trader = ensure_signed(origin)?;
-		// TODO: Do the order logic for the given limit order.
-
-		Ok(())
-		}
-
-		// This function can be used to submit market orders
-		#[weight = 10000]
-		pub fn submit_market_order(origin,
-		  order_type: engine::OrderType,
-		  price: u128,
-		  quantity: u128 ) -> dispatch::DispatchResult{
-		let _trader = ensure_signed(origin)?;
-		// TODO: Do the order logic for the given market order.
-		Ok(())
-		}
-
-		// This function can be used to submit advanced orders
-		#[weight = 10000]
-		pub fn submit_advanced_order(origin,
-		  order_type: engine::OrderType,
-		  price: u128,
-		  quantity: u128 ) -> dispatch::DispatchResult{
-		let _trader = ensure_signed(origin)?;
-		// TODO: Do the order logic for the given advanced order.
-		Ok(())
-		}
-
-		// This function can be used to cancel orders
-		#[weight = 10000]
-		pub fn cancel_order(origin, order_id: Vec<u8>) -> dispatch::DispatchResult{
-		let _trader = ensure_signed(origin)?;
-		// TODO: Do the cancel order logic for the given orderID.
-		Ok(())
-		}
 	}
 }
 
+#[derive(Encode, Decode, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub enum OrderType{
+    BidLimit,
+    BidMarket,
+    AskLimit,
+
+}
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Order<T> where T: Trait {
+    id: T::Hash,
+    trading_pair: T::Hash,
+    trader: T::AccountId,
+    price: FixedU128,
+    quantity: FixedU128,
+    order_type: OrderType
+}
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct LinkedPriceLevel<T> where T: Trait {
+    next: Option<FixedU128>,
+    prev: Option<FixedU128>,
+    orders: VecDeque<Order<T>>
+}
+
+impl<T> Default for LinkedPriceLevel<T> where T: Trait{
+    fn default() -> Self {
+        LinkedPriceLevel{
+            next: None,
+            prev: None,
+            orders: Default::default()
+        }
+    }
+}
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Orderbook<T> where T: Trait {
+    base_asset_id: T::AssetId,
+    quote_asset_id: T::AssetId,
+    best_bid_price: FixedU128,
+    best_ask_price: FixedU128
+}
+
+impl<T> Default for Orderbook<T> where T: Trait{
+    fn default() -> Self {
+        Orderbook{
+            base_asset_id: 0.into(),
+            quote_asset_id: 0.into(),
+            best_bid_price: FixedU128::from(0),
+            best_ask_price: FixedU128::from(0)
+        }
+    }
+}
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct MarketData{
+    // Lowest price at which the trade was executed in a block.
+    low: FixedU128,
+    // Highest price at which the trade was executed in a block.
+    high: FixedU128,
+    // Total volume traded in a block.
+    volume: FixedU128
+}
+
 impl<T: Trait> Module<T> {
-    // fn encode_and_update_nonce() -> Vec<u8> {
-    //     let nonce = Nonce::get();
-    //     Nonce::put(nonce.wrapping_add(1));
-    //     nonce.encode()
-    // }
+
 }
